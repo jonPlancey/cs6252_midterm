0f9c24df1855c00bb0b2fe2e32b046dfe4f8e794
jim perry jr - m.s.a.c.s. <jperry11@my.westga.edu>
2016-02-21 17:11:06 -0500

    # sql design, correcting design

---
+++ database/jimperryjr.sql
@@ -0,0 +1,42 @@
+/*create database*/
+DROP DATABASE IF EXISTS
+  jim_team_member;
+CREATE DATABASE IF NOT EXISTS jim_team_member;
+USE
+  jim_team_member;
+
+ 
+ 
+/*create tables*/
+CREATE TABLE team (
+	team_id				int				auto_increment,
+	team_name			varchar(255)	not null 	unique,
+	team_description	varchar(500)	not null,	
+	
+	primary key (team_id)
+);
+
+
+/*create tables*/
+CREATE TABLE member (
+	team_id				int				not null,
+	member_id			int				auto_increment,	
+	member_name			varchar(255)	not null,
+	member_birthday		date			not null,
+	
+	foreign key (team_id) references artist(team_id),
+	primary key (member_id),
+	
+	unique index id_artist (id_artist),	
+);
+
+
+
+/*create users*/
+create user if not exists artist@192.185.39.71 identified by 'music2015'; 
+
+grant select, insert, update 
+on *.*
+to 'artist'@'localhost' require none;
+
+ 
