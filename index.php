b08c6e4f7cac1b7e76a99c8239d697d21cca372c
jim perry jr - m.s.a.c.s. <jperry11@my.westga.edu>
2016-02-22 06:36:48 -0500

    + added index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php include 'view/template/header.php'; ?>
+<main>
+    <h1>Menu</h1>
+    <ul>
+        <li>
+            <a href="controller/product_manager">Team Manager</a>
+        </li>
+        <li>
+            <a href="controller/product_catalog">Team Catalog</a>
+        </li>
+    </ul>
+</main>
+<?php include 'view/template/footer.php'; ?>
